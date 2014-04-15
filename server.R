4c3c553a29c5afad3dbfda637aea7f57f158f9af
Maarten Vermeyen <maarten.vermeyen@rwo.vlaanderen.be>
2014-04-15 12:12:28 +0200

    Initial commit

---
+++ server.R
@@ -0,0 +1,79 @@
+library(shiny)
+
+shinyServer(function(input,output) {
+  
+  load_data <- reactive( 
+    function () {
+      t <- read.csv(input$dataset)
+      t$rotatiegroep <- cut(t$ROTATION, seq(0, 180, input$rotatiegroepgrootte))
+    return(t)}
+    )
+
+  get_explanatory <- reactive({
+    d = load_data()
+    if (input$explanatory == 1){
+      d$F_INT
+    }else{
+      d$A_INT
+    }
+  })
+  
+  get_model_formula <- function(explanatory) {
+    d = load_data()
+    if (explanatory == 1){
+      as.formula(d$F_INT ~ d$rotatiegroep)
+    }else{
+      as.formula(d$A_INT ~ d$rotatiegroep)
+    }
+  }
+  
+  aov_model <- reactive(aov(get_model_formula(input$explanatory)))
+  
+  output$histPlot <- renderPlot({
+    hist(
+           get_explanatory(), 
+           main="Histogram van het aantal gedetecteerde sporen",
+           ylab="Frequentie",
+           xlab="Aantal gedetecteerde sporen",
+           prob=TRUE
+    )
+    
+    if (input$normaal) curve(dnorm(x,
+                                      mean=mean(get_explanatory()), 
+                                      sd=sd(get_explanatory())
+                                   ), 
+                                   col="darkblue",
+                                   lwd=2,
+                                   add=TRUE,
+                                   yaxt="n"
+                                   )
+  
+    if (input$densiteit) lines(density(get_explanatory()), col="red")
+    
+  })
+  output$qqPlot <- renderPlot({
+    qqnorm(get_explanatory(), main="Kwantiel-Kwantiel-plot van aantal gedetecteerde sporen")
+    qqline(get_explanatory(), col="red")
+  })
+  
+  output$boxPlot <- renderPlot({
+    boxplot(get_model_formula(input$explanatory), notch=TRUE,las=2, main="Boxplot van aantal gedetecteerde sporen per rotatiegroep")
+  })
+  
+  output$anovaSummary <- renderTable({
+    summary(aov_model())
+    })
+  
+  output$anovaPlot <- renderPlot({
+    par(mfrow=c(2,2))
+    plot(aov_model())
+    par(mfrow=c(1,1))
+  })
+  
+  output$TukeyHSD <- renderPlot({
+    par(mar=c(5,9,4,1)+.1)
+    plot(TukeyHSD(aov_model()), las=1, cex.axis=0.65)
+  }, height=2000)
+  
+  output$tabel <- renderDataTable({ load_data() })
+})
---
+++ ui.R
@@ -0,0 +1,47 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  
+  headerPanel("Simulaties"),
+  sidebarLayout(
+  sidebarPanel(
+    selectInput("dataset", "Kies een dataset:", 
+                choices = list.files("./data", full.names=TRUE)),
+    radioButtons("explanatory", "Verklarende variabele",
+                 choices = list("Aantal" = 1, "Oppervlakte" = 2), selected = 1),
+  
+    conditionalPanel(condition="input.conditionedPanels == 'Histogram'",
+    checkboxInput("normaal", "Normaalverdeling weergeven", TRUE),
+    checkboxInput("densiteit", "Densiteitscurve weergeven", TRUE)
+    ),
+    sliderInput("rotatiegroepgrootte", "Grootte van de roatatiegroepen:", 5, 60, 10, step=5)
+  ),
+    
+  
+  mainPanel(
+    tabsetPanel(
+    tabPanel(
+               'Histogram',
+               h3('Histogram'),
+               plotOutput("histPlot"),
+               h3('Kwantiel-Kwantielplot'),
+               plotOutput("qqPlot")
+      ),
+    tabPanel(
+      'Boxplot',
+      h3('Boxplot'),
+       plotOutput("boxPlot")
+    ),
+    tabPanel(
+      'Anova',
+      h3('Anova tabel'),
+      tableOutput("anovaSummary"),
+      h3('Anova plot'),
+      plotOutput("anovaPlot"),
+      h3('Tukey Honest Significant Difference test'),
+      plotOutput("TukeyHSD")
+      ),
+    tabPanel('brondata', dataTableOutput('tabel')),
+    id = "conditionedPanels"
+  )))
+))
