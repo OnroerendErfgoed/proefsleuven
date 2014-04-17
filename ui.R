cc0122527e0280ee5b0177c56499472bdc0a588b
Maarten Vermeyen <maarten.vermeyen@rwo.vlaanderen.be>
2014-04-17 19:26:42 +0200

    added navbar

--- ui.R
+++ ui.R
@@ -1,54 +1,54 @@
 library(shiny)
 
-shinyUI(fluidPage(
+shinyUI(navbarPage('Simulaties',
   
-  headerPanel("Simulaties"),
+  tabPanel('Analyse',
   
-  sidebarLayout(
-    sidebarPanel(
-      selectInput("dataset", "Kies een dataset:", 
-                  choices = list.files("./data", full.names=TRUE)),
-      radioButtons("explanatory", "Verklarende variabele",
-                   choices = list("Aantal" = 1, "Oppervlakte" = 2), selected = 1),
-      
-      conditionalPanel(condition="input.conditionedPanels == 'Histogram'",
-                       p('Opties'),
-                       checkboxInput("normaal", "Normaalverdeling weergeven", TRUE),
-                       checkboxInput("densiteit", "Densiteitscurve weergeven", TRUE)
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("dataset", "Kies een dataset:", 
+                    choices = list.files("./data", full.names=TRUE)),
+        radioButtons("explanatory", "Verklarende variabele",
+                     choices = list("Aantal" = 1, "Oppervlakte" = 2), selected = 1),
+        
+        conditionalPanel(condition="input.conditionedPanels == 'Histogram'",
+                         p('Opties'),
+                         checkboxInput("normaal", "Normaalverdeling weergeven", TRUE),
+                         checkboxInput("densiteit", "Densiteitscurve weergeven", TRUE)
+        ),
+        conditionalPanel(condition="input.conditionedPanels != 'Histogram'",
+                         sliderInput("rotatiegroepgrootte", "Grootte van de roatatiegroepen:", 5, 60, 10, step=5)
+        )
       ),
-      conditionalPanel(condition="input.conditionedPanels != 'Histogram'",
-                       sliderInput("rotatiegroepgrootte", "Grootte van de roatatiegroepen:", 5, 60, 10, step=5)
-      )
-    ),
-    
-    mainPanel(
-      tabsetPanel(
-        
-        tabPanel(
-          'Histogram',
-          h3('Histogram'),
-          plotOutput("histPlot"),
-          h3('Kwantiel-Kwantielplot'),
-          plotOutput("qqPlot")
-        ),
-        
-        tabPanel(
-          'Boxplot',
-          h3('Boxplot'),
-          plotOutput("boxPlot")
-        ),
-        
-        tabPanel(
-          'Anova',
-          h3('Anova tabel'),
-          tableOutput("anovaSummary"),
-          h3('Anova plot'),
-          plotOutput("anovaPlot"),
-          h3('Tukey Honest Significant Difference test'),
-          plotOutput("TukeyHSD")
-        ),
-        
-        tabPanel('brondata', dataTableOutput('tabel')),
-        id = "conditionedPanels"
-      )))
-))
+      
+      mainPanel(
+        tabsetPanel(
+          
+          tabPanel(
+            'Histogram',
+            h3('Histogram'),
+            plotOutput("histPlot"),
+            h3('Kwantiel-Kwantielplot'),
+            plotOutput("qqPlot")
+          ),
+          
+          tabPanel(
+            'Boxplot',
+            h3('Boxplot'),
+            plotOutput("boxPlot")
+          ),
+          
+          tabPanel(
+            'Anova',
+            h3('Anova tabel'),
+            tableOutput("anovaSummary"),
+            h3('Anova plot'),
+            plotOutput("anovaPlot"),
+            h3('Tukey Honest Significant Difference test'),
+            plotOutput("TukeyHSD")
+          ),
+          
+          tabPanel('brondata', dataTableOutput('tabel'), id="Brondata"),
+          id = "conditionedPanels"
+        )))
+)))
