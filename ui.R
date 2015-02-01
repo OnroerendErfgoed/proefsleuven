c5005978c273d5a4cb1cea6c9124fb6d7c770a26
Maarten Vermeyen <vermeyen.m@gmail.com>
2015-02-01 19:24:14 +0100

    Opkuis ui.r

--- ui.R
+++ ui.R
@@ -1,76 +1,77 @@
 library(shinydashboard)
 
-dashboardPage(
+header <- dashboardHeader(title='Simulaties')
+
+sidebar <- dashboardSidebar(
+              selectInput("dataset", "Kies een dataset", choices = list.files("./data", full.names=TRUE)),
+              radioButtons(
+                  "explanatory", 
+                  "Verklarende variabele",
+                  choices = list("Aantal" = 1, "Oppervlakte" = 2),
+                  selected = 1),
+              sidebarMenu(
+                  menuItem('Histogram', tabName='histogram'),
+                  menuItem('Anova', tabName='anova'),
+                  menuItem('Boxplot', tabName='boxplot'),
+                  menuItem('Brondata', tabName='brondata')),
+              downloadButton('downloadData', 'Download brondata'))
   
-  dashboardHeader(title='Simulaties'),
-  dashboardSidebar(
-                      selectInput("dataset", "Kies een dataset", choices = list.files("./data", full.names=TRUE)),
-                      radioButtons(
-                        "explanatory", 
-                        "Verklarende variabele",
-                        choices = list("Aantal" = 1, "Oppervlakte" = 2),
-                        selected = 1
-                      ),
-                      sidebarMenu(
-                        menuItem('Histogram', tabName='histogram'),
-                        menuItem('Anova', tabName='anova'),
-                        menuItem('Boxplot', tabName='boxplot'),
-                        menuItem('Brondata', tabName='brondata')
-                      ),
-                      downloadButton('downloadData', 'Download brondata')
-                      ),
-  dashboardBody(
-                tabItems(
-                  tabItem(tabName='histogram',
-                          fluidRow(
-                            box(title='QQ-plot', solidHeader=TRUE, status = "primary", plotOutput("qqPlot")),
-                            box(title='Histogram', solidHeader=TRUE, status = "primary", plotOutput("histPlot", height = '100%'),
-                                checkboxInput("normaal", "Normaalverdeling weergeven", TRUE),
-                                checkboxInput("densiteit", "Densiteitscurve weergeven", TRUE))
-                          )
-                  ),
+body <-dashboardBody(
+          tabItems(
+            
+            tabItem(tabName='histogram',
+              fluidRow(
+                box(title='QQ-plot', solidHeader=TRUE, status = "primary", plotOutput("qqPlot")),
+                box(
+                  title='Histogram',
+                  solidHeader=TRUE,
+                  status = "primary",
+                  plotOutput("histPlot", height = '100%'),
+                  checkboxInput("normaal", "Normaalverdeling weergeven", TRUE),
+                  checkboxInput("densiteit", "Densiteitscurve weergeven", TRUE)))),
                   
-                  tabItem(tabName='boxplot',
-                          fluidRow(
-                          box(title='Boxplot', solidHeader=TRUE, status = "primary", width=12, 
-                              plotOutput("boxPlot", height='100%'), 
-                              sliderInput("rotatiegroepgrootte", 
-                                          "Grootte van de roatatiegroepen", 5, 60, 10, step=5)))
-                  ),
-                  tabItem(tabName='anova',
-                          fluidRow(
-                                column(width=6,
-                                        box(
-                                         title='Anova samenvatting',
-                                         width=NULL,
-                                         solidHeader=TRUE, 
-                                         status='primary',
-                                         tableOutput("anovaSummary")),
-                                        box(title='Anova plot',
-                                            width=NULL,
-                                            solidHeader=TRUE,
-                                            status='primary',
-                                            plotOutput("anovaPlot"))
-                                    ),
-                                column(width=6,
-                                        box(title="Tukey's Honest Significant Difference", 
-                                            width=NULL,
-                                            solidHeader=TRUE, 
-                                            status='primary', 
-                                            plotOutput("TukeyHSD"))
-                                )
-                          )
-                  ),
-                  tabItem(tabName='brondata',
-                        fluidRow(
-                          box(title='Brondata', 
-                              width=NULL,
-                              solidHeader=TRUE, 
-                              status='primary', dataTableOutput('tabel'))
-                        )
-                     )
-                )                
-  )
-)
+            tabItem(tabName='boxplot',
+              fluidRow(
+                box(
+                  title='Boxplot',
+                  solidHeader=TRUE,
+                  status = "primary",
+                  width=12, 
+                  plotOutput("boxPlot", height='100%'), 
+                  sliderInput("rotatiegroepgrootte", "Grootte van de roatatiegroepen", 5, 60, 10, step=5)))),
+            
+            tabItem(tabName='anova',
+              fluidRow(
+                
+                column(width=6,
+                  box(
+                    title='Anova samenvatting',
+                    width=NULL,
+                    solidHeader=TRUE, 
+                    status='primary',
+                    tableOutput("anovaSummary")),
+                  box(
+                    title='Anova plot',
+                    width=NULL,
+                    solidHeader=TRUE,
+                    status='primary',
+                    plotOutput("anovaPlot"))),
+                
+                column(width=6,
+                  box(
+                    title="Tukey's Honest Significant Difference", 
+                    width=NULL,
+                    solidHeader=TRUE, 
+                    status='primary', 
+                    plotOutput("TukeyHSD"))))),
+            
+            tabItem(tabName='brondata',
+              fluidRow(
+                box(
+                  title='Brondata', 
+                  width=NULL,
+                  solidHeader=TRUE, 
+                  status='primary',
+                  dataTableOutput('tabel'))))))
   
-  
+dashboardPage(header, sidebar, body)
